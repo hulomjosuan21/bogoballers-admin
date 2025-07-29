a980b4a27d0fb1113c8d27b8922cae9d51099108
Josuan Leonardo Hulom <110836440+hulomjosuan21@users.noreply.github.com>
2025-07-29 23:05:00 +0800

    added bracket structure page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import LeagueAdminLayout from './layouts/LeagueAdminLayout'
 import DashboardPage from './pages/league-administrator/DashboardPage'
 import TeamSubmissionPage from './pages/league-administrator/TeamSubmissionPage'
+import BracketStructurePage from './pages/league-administrator/bracket/structurePage'
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/league-administrator" element={<LeagueAdminLayout />}>
           <Route index element={<DashboardPage />} />
           <Route path="team-submission" element={<TeamSubmissionPage />} />
+          <Route path="bracket/structure" element={<BracketStructurePage />} />
         </Route>
       </Routes>
     </>
---
+++ src/pages/league-administrator/bracket/structurePage.tsx
@@ -0,0 +1,7 @@
+export default function BracketStructurePage() {
+    return (
+        <main>
+            <h1 className="text-2xl font-bold">Bracket Structure</h1>
+        </main>
+    )
+}
