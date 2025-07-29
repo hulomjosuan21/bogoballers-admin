ac3c130229f33a7aa38c33aa918bfaa73e1fcf52
Josuan Leonardo Hulom <110836440+hulomjosuan21@users.noreply.github.com>
2025-07-29 22:59:18 +0800

    added league admin helmet

--- src/layouts/LeagueAdminLayout.tsx
+++ src/layouts/LeagueAdminLayout.tsx
@@ -1,16 +1,23 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { Outlet } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 export default function LeagueAdminLayout() {
     return (
-        <SidebarProvider>
-            <main className="flex h-screen w-screen overflow-hidden">
-                <AppSidebar />
-                <main className="flex-1 h-full w-full overflow-auto">
-                    <Outlet />
+        <>
+            <Helmet>
+                <title>BogoBallers | League Administrator</title>
+                <meta name="description" content="Manage your league settings and submissions here." />
+            </Helmet>
+            <SidebarProvider>
+                <main className="flex h-screen w-screen overflow-hidden">
+                    <AppSidebar />
+                    <main className="flex-1 h-full w-full overflow-auto">
+                        <Outlet />
+                    </main>
                 </main>
-            </main>
-        </SidebarProvider>
+            </SidebarProvider>
+        </>
     )
 }
