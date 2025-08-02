cdcade0af24b307ae12865ebf1f9c76c17b16605
Josuan Leonardo Hulom <110836440+hulomjosuan21@users.noreply.github.com>
2025-08-02 08:42:52 +0800

    added dialog

---
+++ src/pages/league-administrator/team/store.ts
@@ -0,0 +1,3 @@
+import { createDialogStore } from "@/stores/dialogStore";
+
+export const useDashboardDialog = createDialogStore()
--- src/stores/dialogStore.ts
+++ src/stores/dialogStore.ts
@@ -1,17 +1,19 @@
-import { create } from 'zustand'
+import { create } from 'zustand';
 
 type DialogState<T = void> = {
-  isOpen: boolean
-  data: T | null
-  dialogOpen: (data?: T) => void
-  dialogClose: () => void
-}
+  isOpen: boolean;
+  data: T | null;
+  dialogOpen: (data?: T) => void;
+  dialogClose: () => void;
+};
 
 export function createDialogStore<T = void>() {
   return create<DialogState<T>>((set) => ({
     isOpen: false,
     data: null,
-    dialogOpen: (data?: T) => set({ isOpen: true, data: data ?? null }),
-    dialogClose: () => set({ isOpen: false, data: null }),
-  }))
+    dialogOpen: (data?: T) =>
+      set({ isOpen: true, data: data ?? null }),
+    dialogClose: () =>
+      set({ isOpen: false, data: null }),
+  }));
 }
