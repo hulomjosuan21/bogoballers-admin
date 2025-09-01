d19b9a8727600fcd057502efdb5b96bc17002a7f
Josuan Leonardo Hulom <hulomjosuan@gmail.com>
2025-09-01 09:53:40 +0800

    add loading state in search

--- src/pages/public/SearchScreen.tsx
+++ src/pages/public/SearchScreen.tsx
@@ -1,10 +1,11 @@
 import { Input } from "@/components/ui/input";
 import { SearchService } from "@/service/search-service";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useTransition } from "react";
 import { useNavigate } from "react-router-dom";
-import { SearchIcon } from "lucide-react";
+import { Loader2Icon, SearchIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import type { QueryResultWrapper } from "@/types/search-results";
+import { useErrorToast } from "@/components/error-toast";
 
 export default function SearchScreen() {
   const [queryResult, setQueryResult] = useState<QueryResultWrapper | null>(
@@ -13,11 +14,20 @@ export default function SearchScreen() {
   const [searchQuery, setSearchQuery] = useState("");
   const inputRef = useRef<HTMLInputElement>(null);
   const navigate = useNavigate();
+  const handleError = useErrorToast();
+  const [isPending, startTransition] = useTransition();
 
   const handleSearch = async () => {
     if (searchQuery.trim() === "") return;
-    const result = await SearchService.searchEntity(searchQuery);
-    setQueryResult(result);
+
+    startTransition(async () => {
+      try {
+        const result = await SearchService.searchEntity(searchQuery);
+        setQueryResult(result);
+      } catch (e) {
+        handleError(e);
+      }
+    });
   };
 
   useEffect(() => {
@@ -42,7 +52,11 @@ export default function SearchScreen() {
             }}
           />
           <div className="text-muted-foreground/80 pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-2 peer-disabled:opacity-50">
-            <SearchIcon size={16} />
+            {isPending ? (
+              <Loader2Icon size={16} className="animate-spin" />
+            ) : (
+              <SearchIcon size={16} />
+            )}
           </div>
         </div>
         <Button variant="ghost" size="sm" onClick={() => navigate(-1)}>
